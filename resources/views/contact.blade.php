70f94ca71375812a22f72331d051907ce752dabc
techworker <benjaminansbach@gmail.com>
2018-12-13 23:05:41 +0100

    Changed contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,37 +11,15 @@
     <div class="row">
         <div class="col-md-6">
             <div class="form-group">
-                <label for="form_name">Firstname *</label>
+                <label for="form_name">Name *</label>
                 <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required.">
                 <div class="help-block with-errors"></div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
-                <label for="form_lastname">Lastname *</label>
-                <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required.">
-                <div class="help-block with-errors"></div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group">
                 <label for="form_email">Email *</label>
                 <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
-                <div class="help-block with-errors"></div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="form_need">Please specify your need *</label>
-                <select id="form_need" name="need" class="form-control" required="required" data-error="Please specify your need.">
-                    <option value=""></option>
-                    <option value="Request quotation">Request quotation</option>
-                    <option value="Request order status">Request order status</option>
-                    <option value="Request copy of an invoice">Request copy of an invoice</option>
-                    <option value="Other">Other</option>
-                </select>
                 <div class="help-block with-errors"></div>
             </div>
         </div>
