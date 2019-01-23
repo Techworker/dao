204024f1755604b0165e6c2b3193f5f056d6bbc0
techworker <benjaminansbach@gmail.com>
2019-01-23 13:25:48 +0100

    A

--- resources/views/foundation-table.blade.php
+++ resources/views/foundation-table.blade.php
@@ -16,7 +16,7 @@
             <td>{{date('Y-m-d H:i:s', $payment->time)}}</td>
             <td>{{$payment->to_pasa}}</td>
             <td>{{$payment->amount}}</td>
-            <td style="word-break: break-all">{{wordwrap($payment->payload, 100, "<br />"}}</td>
+            <td style="word-break: break-all">{{wordwrap($payment->payload, 100, "<br />")}}</td>
             <td>
                 @if($payment->contract !== null)
                     {{$payment->contract->proposal->title}}
