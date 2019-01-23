dd0014c4907126fb8f577a8acddc45b180b1da81
techworker <benjaminansbach@gmail.com>
2019-01-23 13:29:48 +0100

    A

--- resources/views/foundation-table.blade.php
+++ resources/views/foundation-table.blade.php
@@ -6,7 +6,7 @@
         <th>To</th>
         <th>Amount</th>
         <th>Payload</th>
-        <th>Project</th>
+        <th>Project / Contractor</th>
     </tr>
     </thead>
     <tbody>
@@ -16,10 +16,11 @@
             <td>{{date('Y-m-d H:i:s', $payment->time)}}</td>
             <td>{{$payment->to_pasa}}</td>
             <td>{{$payment->amount}}</td>
-            <td style="word-break: break-all">{{wordwrap($payment->payload, 100, "<br />")}}</td>
+            <td style="word-break: break-all">{{wordwrap($payment->payload, 60, "<br />")}}</td>
             <td>
                 @if($payment->contract !== null)
-                    {{$payment->contract->proposal->title}}
+                    <a href="{{\App\Http\Actions\Contractor\ShowAction::route(['contractor' => $payment->contract->contractor, 'slug' => $payment->contract->contractor->slug])}}" class="font-weight-bold">{{$payment->contract->contractor->public_name}}</a><br />
+                        <a href="{{\App\Http\Actions\Profile\Proposal\ShowFormAction::route(['proposal' => $payment->contract->proposal])}}" class="font-weight-bold">{{$payment->contract->proposal->title}}
                 @else
                     Unknown
                 @endif
